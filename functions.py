18f4fbe9c09fb4b953ac0aff722783c84037c409
Jomi :) <92127962+Samuriot@users.noreply.github.com>
2023-08-11 16:20:47 -0700

    updated api calls to function properly

--- functions.py
+++ functions.py
@@ -1,6 +1,15 @@
 # managing url extension
 
 from enum import Enum
+import requests
+import json
+
+api_key = "HDEV-183d6f00-e6ce-40db-adf1-536792261886"
+url = "https://api.henrikdev.xyz"
+
+headers = {
+    "Authorization": api_key,
+}
 
 def chooseType():
     choice = input("Select which type of request you would like: ")
@@ -11,18 +20,37 @@ def chooseType():
             return "/valorant/v1/account/"
         # competitive match history
         case 2:
-            return "/valorant/v1/lifetime/mmr-history/"
+            return "/valorant/v1/by-puuid/lifetime/mmr-history/"
         # previous match history
         case 3:
-            return "/valorant/v1/lifetime/matches/"
+            return "/valorant/v1/by-puuid/mmr/"
         # get current featured shop
         case 4:
             return "/valorant/v1/store-featured/"
         case _:
             return "/"
-
+        
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+    
 def getNameTag():
     name = input("Please input your Riot ID: ")
+    region = input("Please select your region: ")
     tag = name.partition("#")
-    return {1:tag[0], 2:tag[2]}
+    return [tag[0], tag[2], region]
 
+def getNameID(obj):
+    web = url + "/valorant/v1/account/"
+    web += obj[0] + '/' + obj[1]
+    print(web)
+    request = requests.get(web, headers=headers)
+    text = json.dumps(request.json(), sort_keys = True, indent = 4)
+    data = json.loads(text)
+    jprint(data)
+    return data["data"]["puuid"]
+
+def obtainRank(json_parse):
+    rank = json_parse["data"]["currenttierpatched"]
+    currentRR = json_parse["data"]["elo"] % 100
+    return [rank, currentRR]
--- setup.py
+++ setup.py
@@ -3,29 +3,23 @@ import valo_api as val
 import json
 import functions as f
 
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-api_key = "HDEV-183d6f00-e6ce-40db-adf1-536792261886"
-url = "https://staging-api.henrikdev.xyz"
-
-name = f.getNameTag()
-urlExt = f.chooseType()
-
-url = url + urlExt
-url = url + name[1] + "/" + name[2]
-
-print(url)
+api_key = input("Please enter your api key: ")
+url = "https://api.henrikdev.xyz"
 
 headers = {
     "Authorization": api_key,
 }
 
+temp = f.getNameTag();
+player_uid = f.getNameID(temp);
+url_extension = f.chooseType();
+
+url += url_extension + "na/" + player_uid
+print(url)
 response = requests.get(url, headers=headers)
 
 # Process the response here
-
 text = json.dumps(response.json(), sort_keys = True, indent = 4)
 data = json.loads(text)
-print(data)
+
+print(f.obtainRank(data))
